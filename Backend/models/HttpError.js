c9899574ed81be0f13941dd4964aaa18252c2307
Bharath T <149136950+tbharath03@users.noreply.github.com>
2024-07-01 22:09:37 +0530

    Update HttpError.js

--- Backend/models/HttpError.js
+++ Backend/models/HttpError.js
@@ -2,7 +2,7 @@
 class HttpError extends Error {
     constructor(message, errorCode) {
         super(message);
-        this.code = errorCode;
+        this.code = errorCode in http.STATUS_CODES? errorCode:500;
     }
 }
 
